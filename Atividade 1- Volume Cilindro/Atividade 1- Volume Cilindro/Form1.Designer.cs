2be816fcf9217cdf2de84f3f8491da58ad6e7567
Jessica Santos <70185032+jessiemdsantos@users.noreply.github.com>
2020-09-21 11:10:33 -0300

    Add files via upload

---
+++ Atividade 1- Volume Cilindro/Atividade 1- Volume Cilindro/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Atividade_1__Volume_Cilindro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.bntCalc = new System.Windows.Forms.Button();
+            this.bntLimpar = new System.Windows.Forms.Button();
+            this.bntSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(156, 38);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(190, 22);
+            this.txtAltura.TabIndex = 0;
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRaio_KeyPress);
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(156, 94);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(190, 22);
+            this.txtRaio.TabIndex = 1;
+            this.txtRaio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRaio_KeyPress);
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(156, 145);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(190, 22);
+            this.txtVolume.TabIndex = 2;
+            // 
+            // bntCalc
+            // 
+            this.bntCalc.Location = new System.Drawing.Point(38, 248);
+            this.bntCalc.Name = "bntCalc";
+            this.bntCalc.Size = new System.Drawing.Size(145, 51);
+            this.bntCalc.TabIndex = 3;
+            this.bntCalc.Text = "Calcular";
+            this.bntCalc.UseVisualStyleBackColor = true;
+            this.bntCalc.Click += new System.EventHandler(this.bntCalc_Click);
+            // 
+            // bntLimpar
+            // 
+            this.bntLimpar.Location = new System.Drawing.Point(219, 248);
+            this.bntLimpar.Name = "bntLimpar";
+            this.bntLimpar.Size = new System.Drawing.Size(145, 51);
+            this.bntLimpar.TabIndex = 4;
+            this.bntLimpar.Text = "Limpar";
+            this.bntLimpar.UseVisualStyleBackColor = true;
+            this.bntLimpar.Click += new System.EventHandler(this.bntLimpar_Click);
+            // 
+            // bntSair
+            // 
+            this.bntSair.Location = new System.Drawing.Point(397, 248);
+            this.bntSair.Name = "bntSair";
+            this.bntSair.Size = new System.Drawing.Size(145, 51);
+            this.bntSair.TabIndex = 5;
+            this.bntSair.Text = "Sair";
+            this.bntSair.UseVisualStyleBackColor = true;
+            this.bntSair.Click += new System.EventHandler(this.bntSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Raio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Volume";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 331);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bntSair);
+            this.Controls.Add(this.bntLimpar);
+            this.Controls.Add(this.bntCalc);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.txtAltura);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Button bntCalc;
+        private System.Windows.Forms.Button bntLimpar;
+        private System.Windows.Forms.Button bntSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Atividade 1- Volume Cilindro/Atividade 1- Volume Cilindro/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_1__Volume_Cilindro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCalc_Click(object sender, EventArgs e)
+        {
+            double altura, raio;
+            if (double.TryParse(txtAltura.Text, out altura) &&
+                (double.TryParse(txtRaio.Text, out raio)))
+            {
+                double volume;
+                volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+                txtVolume.Text = volume.ToString("N2");
+            }
+            else
+                MessageBox.Show("Valores Invalidos!");
+        }
+
+        private void bntLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtRaio.Text = "";
+            txtVolume.Text = String.Empty;
+
+            txtRaio.Focus();
+        }
+
+        private void bntSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtRaio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+    }
+}
