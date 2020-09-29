470df78e7362da0547b4abdb1af5c15835ba929a
Jessica Santos <70185032+jessiemdsantos@users.noreply.github.com>
2020-09-29 00:49:58 -0300

    Add files via upload

---
+++ Atividade 4/Atividade 4/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Atividade_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLadoA = new System.Windows.Forms.Label();
+            this.lblLadoB = new System.Windows.Forms.Label();
+            this.lblLadoC = new System.Windows.Forms.Label();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.btnTipo = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLadoA
+            // 
+            this.lblLadoA.AutoSize = true;
+            this.lblLadoA.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLadoA.Location = new System.Drawing.Point(34, 38);
+            this.lblLadoA.Name = "lblLadoA";
+            this.lblLadoA.Size = new System.Drawing.Size(81, 23);
+            this.lblLadoA.TabIndex = 0;
+            this.lblLadoA.Text = "Lado A";
+            // 
+            // lblLadoB
+            // 
+            this.lblLadoB.AutoSize = true;
+            this.lblLadoB.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLadoB.Location = new System.Drawing.Point(34, 114);
+            this.lblLadoB.Name = "lblLadoB";
+            this.lblLadoB.Size = new System.Drawing.Size(81, 23);
+            this.lblLadoB.TabIndex = 1;
+            this.lblLadoB.Text = "Lado B";
+            // 
+            // lblLadoC
+            // 
+            this.lblLadoC.AutoSize = true;
+            this.lblLadoC.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLadoC.Location = new System.Drawing.Point(34, 189);
+            this.lblLadoC.Name = "lblLadoC";
+            this.lblLadoC.Size = new System.Drawing.Size(81, 23);
+            this.lblLadoC.TabIndex = 2;
+            this.lblLadoC.Text = "Lado C";
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(168, 38);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(123, 22);
+            this.txtLadoA.TabIndex = 3;
+            this.txtLadoA.TextChanged += new System.EventHandler(this.txtLadoA_TextChanged);
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(168, 114);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(123, 22);
+            this.txtLadoB.TabIndex = 4;
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(168, 189);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(123, 22);
+            this.txtLadoC.TabIndex = 5;
+            // 
+            // btnTipo
+            // 
+            this.btnTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTipo.Location = new System.Drawing.Point(151, 246);
+            this.btnTipo.Name = "btnTipo";
+            this.btnTipo.Size = new System.Drawing.Size(174, 38);
+            this.btnTipo.TabIndex = 6;
+            this.btnTipo.Text = "Tipo de Triângulo";
+            this.btnTipo.UseVisualStyleBackColor = true;
+            this.btnTipo.Click += new System.EventHandler(this.btnTipo_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(386, 58);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 44);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(386, 150);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 42);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 315);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnTipo);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.lblLadoC);
+            this.Controls.Add(this.lblLadoB);
+            this.Controls.Add(this.lblLadoA);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLadoA;
+        private System.Windows.Forms.Label lblLadoB;
+        private System.Windows.Forms.Label lblLadoC;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button btnTipo;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Atividade 4/Atividade 4/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Atividade_4
+{
+    public partial class Form1 : Form
+    {
+        double A, B, C;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtLadoA.Clear();
+            txtLadoB.Clear();
+            txtLadoC.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnTipo_Click(object sender, EventArgs e)
+        {
+            if (txtLadoA.Text == String.Empty || txtLadoB.Text == String.Empty || txtLadoC.Text == String.Empty)
+            {
+                MessageBox.Show("Espaço não pode ficar em branco!!");
+            }
+            else
+            {
+                if ((double.TryParse(txtLadoA.Text, out A)) &&
+                    (double.TryParse(txtLadoB.Text, out B)) &&
+                    (double.TryParse(txtLadoC.Text, out C)))
+                {
+                    if ((Math.Abs(B - C) < A && A < (B + C)) &&
+                        (Math.Abs(A - C) < B && B < (A + C)) &&
+                        (Math.Abs(A - B) < C && C < (A + B)))
+                    {
+                        if (A == B && B == C)
+                        {
+                            MessageBox.Show("Triângulo Equilátero!");
+                        }
+
+                        else if (A != B & B != C && C != A)
+                        {
+                            MessageBox.Show("Triângulo Escaleno!");
+                        }
+
+                        else
+                        {
+                            MessageBox.Show("Triângulo Isósceles!");
+                        }
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Valores Inválidos!");
+                    }
+                }
+            }
+        }
+
+        private void txtLadoA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
