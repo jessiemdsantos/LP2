c3c0b0f5c7205cd9ee7886dc5328e8927f817730
Jessica Santos <70185032+jessiemdsantos@users.noreply.github.com>
2020-11-17 11:53:35 -0300

    Add files via upload

---
+++ Prova/P0030482011045/P0030482011045/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace P0030482011045
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnVerificar = new System.Windows.Forms.Button();
+            this.LstbxVendas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnVerificar
+            // 
+            this.BtnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnVerificar.Location = new System.Drawing.Point(52, 82);
+            this.BtnVerificar.Name = "BtnVerificar";
+            this.BtnVerificar.Size = new System.Drawing.Size(148, 82);
+            this.BtnVerificar.TabIndex = 0;
+            this.BtnVerificar.Text = "Verificar";
+            this.BtnVerificar.UseVisualStyleBackColor = true;
+            this.BtnVerificar.Click += new System.EventHandler(this.BtnVerificar_Click);
+            // 
+            // LstbxVendas
+            // 
+            this.LstbxVendas.FormattingEnabled = true;
+            this.LstbxVendas.ItemHeight = 16;
+            this.LstbxVendas.Location = new System.Drawing.Point(316, 15);
+            this.LstbxVendas.Name = "LstbxVendas";
+            this.LstbxVendas.Size = new System.Drawing.Size(262, 244);
+            this.LstbxVendas.TabIndex = 1;
+            this.LstbxVendas.SelectedIndexChanged += new System.EventHandler(this.LstbxVendas_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 277);
+            this.Controls.Add(this.LstbxVendas);
+            this.Controls.Add(this.BtnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnVerificar;
+        private System.Windows.Forms.ListBox LstbxVendas;
+    }
+}
+
---
+++ Prova/P0030482011045/P0030482011045/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482011045
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVerificar_Click(object sender, EventArgs e)
+        {
+            
+            double[,] matrizVendas = new double[5, 4];
+            double totalMes = 0;
+            double totalGeral = 0;
+            string Valor;
+
+            for (int i = 0; i < matrizVendas.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizVendas.GetLength(1); j++)
+                {
+                    while (true)
+                    {
+                        Valor = Interaction.InputBox("Valor referente: \nMês: " + (i + 1) + 
+                            "\nSemana: " + (j + 1), "Valores das vendas");
+
+                        if (double.TryParse(Valor, out matrizVendas[i, j]))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro! Digite um valor valído!");
+                        }
+                    }
+                }
+
+            }
+            
+                     LstbxVendas.Items.Clear();
+                for (int i = 0; i < matrizVendas.GetLength(0); i++)
+                {
+                for (int j = 0; j < matrizVendas.GetLength(1); j++)
+                
+                {
+                LstbxVendas.Items.Add("Total do Mes: " + (i + 1) + " Semana " + (j + 1) + ": " + matrizVendas[i, j].ToString("C2"));
+                totalGeral += matrizVendas[i, j];
+                totalMes += matrizVendas[i, j]; }
+                    LstbxVendas.Items.Add(">>Total do Mês " + (i + 1) + ": " + totalMes.ToString("C2"));
+                    totalMes = 0;
+                    LstbxVendas.Items.Add("-------------------------------");
+            }
+               
+                LstbxVendas.Items.Add(" >>>> Total Geral: " + totalGeral.ToString("C2"));
+        }
+
+        private void LstbxVendas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+              
+
+        }
+    }
+}
